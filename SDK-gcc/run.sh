0a2e7eecaab6bca7aa9db397ad6842e2c1e72fcd
lijiancheng0614 <lijiancheng0614@gmail.com>
2017-04-19 15:42:14 +1000

    Minor updates.

--- SDK-gcc/run.sh
+++ SDK-gcc/run.sh
@@ -9,7 +9,7 @@ cd ../bin
 for CASE_ID in `seq 0 9`;
 do
     mkdir -p 1Intermediate
-    ./cdn ../../case_example/round2/batch3/1Intermediate/case${CASE_ID}.txt 1Intermediate/result${CASE_ID}.txt
+    ./cdn ../../case_example/batch3/1Intermediate/case${CASE_ID}.txt 1Intermediate/result${CASE_ID}.txt
     mkdir -p 2Advanced
-    ./cdn ../../case_example/round2/batch3/2Advanced/case${CASE_ID}.txt 2Advanced/result${CASE_ID}.txt
+    ./cdn ../../case_example/batch3/2Advanced/case${CASE_ID}.txt 2Advanced/result${CASE_ID}.txt
 done
