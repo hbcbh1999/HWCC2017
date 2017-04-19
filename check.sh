cb76ba55bea3cfd13877b4ad0fbac382c6b1f66d
lijiancheng0614 <lijiancheng0614@gmail.com>
2017-04-19 15:40:40 +1000

    Remove round1 data.

--- check.sh
+++ check.sh
@@ -1,10 +1,10 @@
-echo "round2/batch3/1Intermediate"
+echo "batch3/1Intermediate"
 for CASE_ID in `seq 0 9`;
 do
-    python check.py case_example/round2/batch3/1Intermediate/case${CASE_ID}.txt SDK-gcc/bin/1Intermediate/result${CASE_ID}.txt
+    python check.py case_example/batch3/1Intermediate/case${CASE_ID}.txt SDK-gcc/bin/1Intermediate/result${CASE_ID}.txt
 done
-echo "round2/batch3/2Advanced"
+echo "batch3/2Advanced"
 for CASE_ID in `seq 0 9`;
 do
-    python check.py case_example/round2/batch3/2Advanced/case${CASE_ID}.txt SDK-gcc/bin/2Advanced/result${CASE_ID}.txt
+    python check.py case_example/batch3/2Advanced/case${CASE_ID}.txt SDK-gcc/bin/2Advanced/result${CASE_ID}.txt
 done
